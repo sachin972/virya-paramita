7b765959832609899e79e16829b20cc8f372e087
sachin972 <sachintripathi972000@gmail.com>
2025-02-10 01:27:09 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './Components/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
